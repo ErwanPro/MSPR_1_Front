aafbab8ac906fba99b1b0097c4e06782e6a2301e
marcanta <aime.marcant@hotmail.fr>
2021-03-15 15:38:02 +0100

    fetch coupon from api

--- Screens/Coupons.jsx
+++ Screens/Coupons.jsx
@@ -1,21 +1,36 @@
 import React from "react";
-import { FlatList, Text } from "react-native";
+import { FlatList, RefreshControl } from "react-native";
+import { getCoupons } from "../API/Coupon";
+import Coupon from "../Components/Coupon";
 
 class Coupons extends React.Component {
 	constructor(props) {
 		super(props);
 
 		this.state = {
-			Coupons: [],
+			refreshing: false,
+			coupons: [],
 		};
 	}
 
+	async componentDidMount() {
+		const coupons = await getCoupons();
+		this.setState({ coupons: coupons });
+	}
+
+	onRefresh = async () => {
+		const coupons = await getCoupons();
+		this.setState({ coupons: coupons, refreshing: false });
+	};
+
 	render() {
+		getCoupons().then(coupon => console.log(coupon));
 		return (
 			<FlatList
 				style={{ backgroundColor: "white" }}
-				data={this.state.projects}
-				renderItem={({ item, index }) => <Text>ttoto</Text>}
+				data={this.state.coupons}
+				renderItem={({ item, index }) => <Coupon {...item} />}
+				refreshControl={<RefreshControl refreshing={this.state.refreshing} onRefresh={this.onRefresh} />}
 				keyExtractor={item => item._id}
 			/>
 		);
